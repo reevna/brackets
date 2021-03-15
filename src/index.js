3f10593d4d6440b64c5e871d4d3af00af0a6e243
github-reevna <shu-@list.ru>
2021-03-15 10:07:17 +0300

    add: solution for false cases

--- src/index.js
+++ src/index.js
@@ -1,3 +1,16 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  if (str % 2 !=0){
+    return false
+  }
+
+  if (str.endsWith("(") ) {
+    return false
+  }
+  if (str.startsWith(")") || str.startsWith("}") || str.startsWith("]")) {
+    return false
+  }
+ if (str.match('(])') || str.match('(|)')){
+   return false
+ }
+
 }
